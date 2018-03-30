84ddd5c935359bbf8c48d02355868e3999a1bdc7
RCConnolly <ryan.connolly@duke.edu>
2018-03-30 13:48:45 -0400

    currentFM2 works with channels_updated.png

---
+++ examples/currentFM2.cpp
@@ -0,0 +1,193 @@
+/* Main script for running wFM2 for ASV */
+
+#include <iostream>
+#include <fstream>
+#include <array>
+#include <string>
+
+#include <fast_methods/ndgridmap/fmcell.h>
+#include <fast_methods/ndgridmap/ndgridmap.hpp>
+#include <fast_methods/console/console.h>
+
+#include <fast_methods/fm2/fm2.hpp>
+//#include <fast_methods/fm2/wFM2.hpp>
+
+#include <fast_methods/io/maploader.hpp>
+#include <fast_methods/io/gridplotter.hpp>
+#include <fast_methods/io/gridwriter.hpp>
+
+
+// Establish Grid & Path Dimensionality to 2
+constexpr unsigned int nDims = 2;
+typedef nDGridMap<FMCell, nDims> FMGrid;
+typedef typename std::array<double, nDims> Point;
+typedef typename std::array<unsigned int, nDims> Coord;
+typedef typename std::vector<Point> Path;
+
+/* Converts point coordinates from UTM northing & easting in decimal degrees
+ * to image (x,y) coordinates with the bottom left corner of the image being
+ * (0,0) and the x & y axes increasing to the right & upward respectively.
+ * Parameters:
+ *      utm_pt = {northing, easting} coordinates of point
+ *      bottom = northing of image's bottom extent
+ *      left   = easting of image's left extent
+ *      width  = number of pixels that the image is wide
+ *      height = number of pixels that the image is high
+ * Returns: none
+*/
+Coord utm2image(Point & utm_pt, const int bottom, const int left, const int width, const int height) {
+    
+    assert(utm_pt[0] > bottom);
+    assert(utm_pt[1] > left);
+
+    unsigned int y_img = utm_pt[0] - bottom;
+    unsigned int x_img = utm_pt[1] - left;
+
+    if(x_img >= width) {
+        std::cout << "Converted image x-coordinate" << x_img << " out of range\n";
+        //std::cout << "utm[1]: " << utm_pt[1] << "\n";
+        //std::cout << "left: " << left << "\n";
+        exit(EXIT_FAILURE);
+    }
+
+    if(y_img >= height) {
+        std::cout << "Converted image y-coordinate out of range\n";
+        exit(EXIT_FAILURE);
+    }
+
+    Coord pt = {x_img, y_img};
+
+    return pt;
+}
+
+
+/* Computes a 2D path connecting the start & goal points. Each point in the path is
+ * is in image coordinates.
+ * Paramerters:
+ *      start = initial point in image coordinates
+ *      goal = goal point in image coordinates
+ *      filename = name of obstacle image for the path to be computed on
+ *      step = number of image pixels between each point in the path
+*/
+void computePath(Path & path, const Coord & start, const Coord & goal, const std::string filename) {
+    // Loading grid.
+    FMGrid grid_fm2;
+
+    // Solver declaration.
+    Solver<FMGrid> * s = new FM2<FMGrid>("FM2_Dary");
+   
+    // fills in occupancy grid    
+    MapLoader::loadMapFromImg(filename.c_str(), grid_fm2);
+    s->setEnvironment(&grid_fm2);
+    
+    // computes velocities map    
+    s->setInitialAndGoalPoints(start, goal);
+    s->compute();
+
+    std::vector<double> path_vels;
+    s->as<FM2<FMGrid>>()->computePath(&path, &path_vels);
+    GridPlotter::plotArrivalTimesPath(grid_fm2, path);
+
+    // Preventing memory leaks.
+    delete s;
+
+    return;
+}
+
+
+/* Prints each path point to std::out, with coordinates being in Lat/Long
+void printPath(Path * path, const char * outfile) {
+
+}
+*/
+
+
+Point getPoint(std::ifstream * f) {
+    char northing[100];
+    char easting[100];
+
+    f->getline(northing, 100, ' ');
+    f->getline(easting, 100, '\n');
+
+    double iNorthing = atof(northing);
+    double iEasting  = atof(easting);
+
+    Point pt = {iNorthing, iEasting};
+
+    return pt;
+}
+
+void checkZone(const char * pt_zone, const char * img_zone) {
+    if(strcmp(pt_zone, img_zone)) {
+        std::cout << "ERROR: Point UTM zone (" << pt_zone << ")";
+        std::cout << " does not match image zone(" << img_zone << ")\n";
+        exit(EXIT_FAILURE);
+    }
+    
+    return;
+}
+
+// Input format:
+// IMAGE_FILENAME POINTS_FILENAME
+int main(int argc, const char ** argv)
+{
+    // Check command line arguments
+    if(argc < 3) {
+        std::cout << "Not enough arguments given. Use as ./run IMAGE_FILENAME POINTS_FILENAME\n";
+        exit(EXIT_FAILURE);
+    }
+
+    // Parse GeoTiff image
+    ///////////////////////////
+    // NEED TO IMPLEMENT
+    //////////////////////////
+    // hard coding values for channels_updated.png image
+    std::string image_name = argv[1];
+    const int bottom = 3836014;
+    const int left   = 340154;
+    const int width = 1316;
+    const int height = 1023;
+    const char image_zone[] = "18n";
+    
+    // Read POINTS_FILENAME for zone+hemisphere, easting, northing of start & goal
+    std::ifstream points_file(argv[2]);
+    Point start;
+    Point goal;
+    if(points_file.is_open()) {
+        // Set starting point & check its UTM zone
+        char start_zone[4];
+        points_file.get(start_zone, 4); // reads in zone (2 digits, 1 letter)
+        checkZone(start_zone, image_zone);
+        points_file.get(); // moves passed white space after zone
+        start = getPoint(&points_file);
+
+        // Set goal point & check its UTM zone
+        char goal_zone[4];
+        points_file.get(goal_zone, 4);
+        checkZone(goal_zone, image_zone);
+        points_file.get(); // moves passed white space after zone
+        goal  = getPoint(&points_file);
+    }
+    else {
+        std::cout << "Could not open file\n";
+        return EXIT_FAILURE;
+    }
+    points_file.close();
+
+    
+    // Convert start & goal coordinates to image coordinates
+    Coord start_coord = utm2image(start, bottom, left, width, height);
+    Coord goal_coord  = utm2image(goal,  bottom, left, width, height);
+    //std::cout << "start: {" << start_coord[0] << "," << start_coord[1] << "}\n";
+    //std::cout << "goal: {" << goal_coord[0] << "," << goal_coord[1] << "}\n";
+
+    // Compute path
+    Path path;
+    computePath(path, start_coord, goal_coord, image_name);
+
+    // Print path to a file
+    // printPath(path_ptr, "path.txt");
+
+
+    return EXIT_SUCCESS;
+}

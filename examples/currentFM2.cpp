daf26bbda2cd0ba92c4c9e145c38608c05baf704
RCConnolly <ryan.connolly@duke.edu>
2018-03-30 16:57:39 -0400

    currentFM2.cpp now prints UTM path coordinates to path.txt

--- examples/currentFM2.cpp
+++ examples/currentFM2.cpp
@@ -1,6 +1,7 @@
 /* Main script for running wFM2 for ASV */
 
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <array>
 #include <string>
@@ -35,27 +36,27 @@ typedef typename std::vector<Point> Path;
  *      height = number of pixels that the image is high
  * Returns: none
 */
-Coord utm2image(Point & utm_pt, const int bottom, const int left, const int width, const int height) {
+Point utm2image(Point & utm_pt, const double bottom, const double left, const int width, const int height) {
     
     assert(utm_pt[0] > bottom);
     assert(utm_pt[1] > left);
 
-    unsigned int y_img = utm_pt[0] - bottom;
-    unsigned int x_img = utm_pt[1] - left;
+    double  y_img = utm_pt[0] - bottom;
+    double  x_img = utm_pt[1] - left;
 
     if(x_img >= width) {
-        std::cout << "Converted image x-coordinate" << x_img << " out of range\n";
+        //std::cout << "Converted image x-coordinate" << x_img << " out of range\n";
         //std::cout << "utm[1]: " << utm_pt[1] << "\n";
         //std::cout << "left: " << left << "\n";
         exit(EXIT_FAILURE);
     }
 
     if(y_img >= height) {
-        std::cout << "Converted image y-coordinate out of range\n";
+        //std::cout << "Converted image y-coordinate out of range\n";
         exit(EXIT_FAILURE);
     }
 
-    Coord pt = {x_img, y_img};
+    Point pt = {x_img, y_img};
 
     return pt;
 }
@@ -69,7 +70,7 @@ Coord utm2image(Point & utm_pt, const int bottom, const int left, const int widt
  *      filename = name of obstacle image for the path to be computed on
  *      step = number of image pixels between each point in the path
 */
-void computePath(Path & path, const Coord & start, const Coord & goal, const std::string filename) {
+void computePath(Path & path, const Point & start, const Point & goal, const std::string filename) {
     // Loading grid.
     FMGrid grid_fm2;
 
@@ -80,8 +81,12 @@ void computePath(Path & path, const Coord & start, const Coord & goal, const std
     MapLoader::loadMapFromImg(filename.c_str(), grid_fm2);
     s->setEnvironment(&grid_fm2);
     
-    // computes velocities map    
-    s->setInitialAndGoalPoints(start, goal);
+    // computes velocities map 
+    Coord start_coord = {(unsigned int) start[0], (unsigned int) start[1]};
+    Coord goal_coord  = {(unsigned int)  goal[0],  (unsigned int) goal[1]};   
+    //std::cout << "goal coord: " << goal_coord[0] << ", " << goal_coord[1] << "\n";
+    //std::cout << "start coord: " << start_coord[0] <<  ", " << start_coord[1] << "\n";
+    s->setInitialAndGoalPoints(start_coord, goal_coord);
     s->compute();
 
     std::vector<double> path_vels;
@@ -95,11 +100,29 @@ void computePath(Path & path, const Coord & start, const Coord & goal, const std
 }
 
 
-/* Prints each path point to std::out, with coordinates being in Lat/Long
-void printPath(Path * path, const char * outfile) {
+// Prints each path point a file, with coordinates being in Lat/Long
+void printUTMPath(Path & path, const char * fname) {
+    
+    const double bottom = 3836014;
+    const double left   = 340154;
 
+    std::ofstream ofs(fname);
+
+    if(ofs.is_open()) {
+        for(size_t i = 0; i < path.size(); i++) {
+            //convert to utm before printing
+            ofs << "18n "; // zone
+            ofs.precision(10);
+            ofs << (path[i][1]+bottom) << " "; // northing
+            ofs << (path[i][0]+left) << "\n";  // easting
+        }
+    }
+    else {
+        std::cout << "Unable to open file\n";
+        exit(EXIT_FAILURE);
+    }
 }
-*/
+
 
 
 Point getPoint(std::ifstream * f) {
@@ -111,6 +134,8 @@ Point getPoint(std::ifstream * f) {
 
     double iNorthing = atof(northing);
     double iEasting  = atof(easting);
+    //std::cout << "northing: " << iNorthing;
+    //std::cout << ", easting: " << iEasting << "\n";
 
     Point pt = {iNorthing, iEasting};
 
@@ -143,8 +168,8 @@ int main(int argc, const char ** argv)
     //////////////////////////
     // hard coding values for channels_updated.png image
     std::string image_name = argv[1];
-    const int bottom = 3836014;
-    const int left   = 340154;
+    const double bottom = 3836014;
+    const double left   = 340154;
     const int width = 1316;
     const int height = 1023;
     const char image_zone[] = "18n";
@@ -159,6 +184,7 @@ int main(int argc, const char ** argv)
         points_file.get(start_zone, 4); // reads in zone (2 digits, 1 letter)
         checkZone(start_zone, image_zone);
         points_file.get(); // moves passed white space after zone
+        //std::cout << "start point -- ";
         start = getPoint(&points_file);
 
         // Set goal point & check its UTM zone
@@ -166,6 +192,7 @@ int main(int argc, const char ** argv)
         points_file.get(goal_zone, 4);
         checkZone(goal_zone, image_zone);
         points_file.get(); // moves passed white space after zone
+        //std::cout << "end point -- ";
         goal  = getPoint(&points_file);
     }
     else {
@@ -174,19 +201,18 @@ int main(int argc, const char ** argv)
     }
     points_file.close();
 
-    
     // Convert start & goal coordinates to image coordinates
-    Coord start_coord = utm2image(start, bottom, left, width, height);
-    Coord goal_coord  = utm2image(goal,  bottom, left, width, height);
+    Point startImg = utm2image(start, bottom, left, width, height);
+    Point goalImg  = utm2image(goal,  bottom, left, width, height);
     //std::cout << "start: {" << start_coord[0] << "," << start_coord[1] << "}\n";
     //std::cout << "goal: {" << goal_coord[0] << "," << goal_coord[1] << "}\n";
 
     // Compute path
     Path path;
-    computePath(path, start_coord, goal_coord, image_name);
+    computePath(path, startImg, goalImg, image_name);
 
     // Print path to a file
-    // printPath(path_ptr, "path.txt");
+    printUTMPath(path, "path.txt");
 
 
     return EXIT_SUCCESS;
